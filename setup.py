ef69295c05ec7b95de44d8d42747e39dbac8498a
Ale Cunha <ale@rcunha.net>
2019-09-11 22:42:10 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='qbo-file-reader',
+    version='0.0.1',
+    description='Read QBO files',
+    author='Alexandre Cunha',
+    author_email='ale@rcunha.net',
+    license='MIT',
+    packages=['qbo'],
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    url='https://github.com/alercunha/qbo-file-reader',
+)
